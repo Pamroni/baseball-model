6cf0d6ccba9ad560818f3b5c96d855b1feb25f50
Alex Ramponi <alex.a.ramponi@gmail.com>
2024-11-10 20:47:25 -0600

    Working to pull historic data of games

---
+++ nn_model.py
@@ -0,0 +1,18 @@
+import torch
+
+class BaseballModel(torch.nn.Module):
+    def __init__(self, in_features, out_features, layers=[256, 512, 1024, 2048]):
+        super(BaseballModel, self).__init__()
+        layer_sizes = [in_features] + layers
+        model_layers = []
+        for i in range(len(layers)):
+            model_layers.append(torch.nn.Linear(layer_sizes[i], layer_sizes[i+1]))
+            model_layers.append(torch.nn.ReLU()) 
+        
+        # Classifiation layer
+        model_layers.append(torch.nn.Linear(layers[-1], out_features))
+
+        self.network = torch.nn.Sequential(*model_layers)
+
+    def forward(self, x):
+        return self.network(x)
